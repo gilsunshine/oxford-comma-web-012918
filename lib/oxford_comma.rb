2729d932eca9bc8d0ed3c1bf88a7aceb74f4c837
Gil Sunshine <gilsunshine@gmail.com>
2018-01-09 23:03:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  counter = 0
+  sentence = ""
+  if array.length == 1
+    sentence << "#{array.last}"
+  elsif array.length == 2
+    sentence << "#{array.first} and #{array.last}"
+  else
+    while counter < array.length-1
+      sentence << "#{array[counter]}, "
+      counter += 1
+    end
+    sentence << "and #{array.last}"
+  end  
 end
